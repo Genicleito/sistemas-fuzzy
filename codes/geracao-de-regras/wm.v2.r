7b622dd91f6e52fc5328756976b57e0316bd061e
Genicleito Gonçalves <Genicleito@users.noreply.github.com>
2019-07-07 20:18:12 -0300

    :tada: starting version 2 of Wang & Mendel method

---
+++ codes/geracao-de-regras/wm.v2.r
@@ -0,0 +1,50 @@
+source("https://raw.githubusercontent.com/Genicleito/sistemas-fuzzy/master/membership-functions/funcoesPertinenciaFuzzy.r")
+
+# Dataset to use in algorithm
+data = iris[, -ncol(iris)]
+
+######### Functions definition #########
+## None
+
+
+############### Wang & Mendel algorithm ############### 
+
+# Step 1. Divide the input and output spaces into Fuzzy Regions
+
+# Domain interval definition
+domain.interval = apply(data, 2, range)
+row.names(domain.interval) = c("min", "max")
+
+# Fuzzy regions
+
+# Function to get points of fuzzy triangular functions
+triangular.fuzzy.regions.points = function(values, domain.interval, n.regions) {
+  min.value = domain.interval[1, 1]
+  max.value = domain.interval[2, 1]
+  
+  interval.length = abs(max.value - min.value)
+  
+  dist.inter.points = interval.length / (n.regions - 1)
+  points.fuzzy.regions = seq(min.value, max.value, dist.inter.points)
+  
+  all.values.points = c()
+  all.degrees = c()
+  for(i in 1:length(points.fuzzy.regions)) {
+    if(i == 1) {
+      all.values.points = c(rep(points.fuzzy.regions[i], 2), points.fuzzy.regions[i + 1])
+      all.degrees = c(1, 1, 0)
+    } else if(i == length(points.fuzzy.regions)) {
+      all.values.points = c(all.values.points, points.fuzzy.regions[i - 1], rep(points.fuzzy.regions[i], 2))
+      all.degrees = c(all.degrees, 0, 1, 1)
+    } else {
+      all.values.points = c(all.values.points, points.fuzzy.regions[(i - 1):(i + 1)])
+      all.degrees = c(all.degrees, 0, 1, 0)
+    }
+  }
+  data.frame(values = all.values.points, degree = all.degrees)
+}
+
+
+# Example: testing the method
+n.regions = 5
+triangular.fuzzy.regions.points(values = data$Sepal.Length, domain.interval = domain.interval, n.regions = n.regions)
