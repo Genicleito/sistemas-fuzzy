ca5755d2c9de79ffab987a1ec84516764d54a394
Genicleito Goncalves <genicleito@live.com>
2019-08-28 22:48:25 -0300

    bug correction :bug:

--- codes/pre-processamento/deleting_special_chars_with_sed_part2.py
+++ codes/pre-processamento/deleting_special_chars_with_sed_part2.py
@@ -9,7 +9,7 @@ seds = [
     "sed -r 's/\\\"|\,//g' /tmp/out_.csv > /tmp/out.csv # remove aspas não escapadas e vírgulas no meio dos campos",
     
     "sed -r 's/\\;/\\\"\\,\\\"/g' /tmp/out.csv > /tmp/out_.csv # volta o separador para , e resgata as aspas do final de cada campo",
-    "sed -r 's/_cidacs_control_quotes_/\"/g' /tmp/out_.csv > /tmp/out.csv # resgata as aspas do início e final das linhas"
+    "sed -r 's/_cidacs_control_quotes_/\\\"/g' /tmp/out_.csv > /tmp/out.csv # resgata as aspas do início e final das linhas"
 ]
 for sed in seds:
     error = False
